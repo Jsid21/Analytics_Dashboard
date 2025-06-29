a67c6c11ed826af2213348db7562a819ae3c46ea
SIDDHESH JOSHI <118831434+Jsid21@users.noreply.github.com>
2025-06-29 22:57:36 +0530

    Delete server/dist directory

--- server/dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const controllers_1 = require("../controllers");
-const router = (0, express_1.Router)();
-router.post('/login', controllers_1.loginUser);
-router.get('/transactions', controllers_1.fetchTransactions);
-router.get('/transactions/csv', controllers_1.generateTransactionCSV);
-exports.default = router;
